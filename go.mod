76b2b5c173217fb9c6cc90920d6965a94075b474
issy20 <gggibraltar86@gmail.com>
2024-04-30 20:34:05 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-tf-demo
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		response := struct {
+			Code    int    `json:"code"`
+			Message string `json:"message"`
+		}{
+			Code:    200,
+			Message: "OK",
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	})
+
+	server.ListenAndServe()
+}
