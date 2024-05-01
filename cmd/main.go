3f5fc960ed90d30ece86b206b17c62ec8d5e7499
issy20 <gggibraltar86@gmail.com>
2024-05-01 20:13:04 +0900

    Refactor health endpoint handler

--- cmd/main.go
+++ cmd/main.go
@@ -15,19 +15,21 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		response := struct {
-			Code    int    `json:"code"`
-			Message string `json:"message"`
-		}{
-			Code:    200,
-			Message: "OK",
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(response)
-	})
+	http.HandleFunc("/health", healthHandler)
 
 	server.ListenAndServe()
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	response := struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}{
+		Code:    200,
+		Message: "OK",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
